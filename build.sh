26e791ff1448f4420f8810c186c0210898260ff2
Joe Tsai <joe.tsai8207@gmail.com>
2025-10-22 10:15:21 +0800

    Add main.c to control number of cpus used for burn test

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ if [ -z "$NDK_ROOT" ]; then
 	exit 1
 fi
 
-$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang cpuburn-a53.S -o stress-neon
+$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang -c func_cpuburn-a53.S -o stress.o
+$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang main.c stress.o -o stress-neon
---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern void stress();
+
+int main(int argc, char *argv[]) {
+	int exit_status;
+	int ncpus = sysconf(_SC_NPROCESSORS_ONLN);
+	int nproc = 1;
+	printf("=== ARM NEON Stress test ===\n");
+
+	if (argc == 1) {
+		printf("Number of processes not specified. Using number of online processors (%d).\n", ncpus);
+		nproc = ncpus;
+	} else {
+		int tmp = atoi(argv[1]);
+		if (tmp > ncpus) {
+			printf("WARN: Specified number of processes (%d) exceeds number of online processors (%d), limits to online processors.\n", tmp, ncpus);
+			nproc = ncpus;
+		} else if (tmp < 1) {
+			printf("WARN: Specified number of processes (%d) is less than 1, limits to 1.\n", tmp);
+			nproc = 1;
+		} else {
+			nproc = tmp;
+		}
+	}
+
+	if (nproc == -1) {
+		fprintf(stderr, "Failed to get number of online processors.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Starting %d processes...\n", nproc);
+	if (nproc < 2) {
+		goto end;
+	}
+
+	for (int i = 1; i < nproc; i++) {
+		pid_t PID = fork();
+		if (PID < 0) {
+			perror("fork()");
+			exit(EXIT_FAILURE);
+		} else if (PID == 0) {
+//			printf("[%d] [Child] Child's PID is %d\n", i, getpid());
+			break;
+		} else if (PID > 0) {
+//			printf("[%d] [Parent] Parent's PID is %d\n", i, getpid());
+		}
+	}
+
+end:
+	printf("Process PID: %d\n", getpid());
+	stress();
+	return 0;
+}
